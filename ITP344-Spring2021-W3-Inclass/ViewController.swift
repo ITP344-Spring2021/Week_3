cfa10f8093b6345fe15e89521f11321f354557b0
Jack Boyuan Xu <boyuanxu@usc.edu>
2021-02-01 19:04:04 -0800

    Initial Commit

---
+++ ITP344-Spring2021-W3-Inclass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITP344-Spring2021-W3-Inclass
+//
+//  Created by Jack Xu on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
